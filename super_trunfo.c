6c189151ca596c59e3ef31c326fe438720a08099
João Paulo Guimarães Leite <neo4jpgl@gmail.com>
2025-07-04 12:54:23 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -68,7 +68,7 @@ int main() {
     printf("Codigo: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("Populacao: %d\n", populacao1);
-    printf("Area: %.2f km²\n", area1);
+    printf("Area: %.2f em km quadrado\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
 
@@ -78,7 +78,7 @@ int main() {
     printf("Codigo: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nomeCidade2);
     printf("Populacao: %d\n", populacao2);
-    printf("Area: %.2f km²\n", area2);
+    printf("Area: %.2f em km quadrado\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
 
